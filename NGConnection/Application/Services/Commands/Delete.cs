472b38d69f1b3893a80740b97c0de37531db5cd3
NavoreGames <navoregames@gmail.com>
2025-01-06 09:49:00 -0300

    Adicionado objeto Delete

---
+++ NGConnection/Application/Services/Commands/Delete.cs
@@ -0,0 +1,24 @@
+﻿namespace NGConnection;
+
+public class Delete : Command
+{
+    public Where Where { get; set; }
+
+    public Delete() { }
+    public Delete(string tableName)
+    {
+        Name = tableName;
+    }
+
+    public override void SetValues(object source)
+    {
+        Name = GetTableName(source);
+    }
+
+    //public override ICommandDml SetCommand(Type connectionType)
+    //{
+    //    Command = @$"DELETE FROM {Table}";
+
+    //    return this;
+    //}
+}
