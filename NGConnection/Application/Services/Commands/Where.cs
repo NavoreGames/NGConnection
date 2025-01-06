6021e0daceefa9adc86404a6d603f429ca933fc5
NavoreGames <navoregames@gmail.com>
2025-01-06 09:54:42 -0300

    Adicionado objeto Where

---
+++ NGConnection/Application/Services/Commands/Where.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using NGConnection.Models;
+
+namespace NGConnection;
+
+public class Where : Command
+{
+    public ExpressionData Expression { get; set; }
+
+}
---
+++ NGConnection/Domain/Models/ExpressionData.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace NGConnection.Models;
+
+public class ExpressionData
+{
+    public ExpressionData ExpressionLeft { get; set; }
+    public ExpressionType ExpressionType { get; set; }
+    public ExpressionData ExpressionRight { get; set; }
+    public Type Type { get; set; }
+    public object Value { get; set; }
+}
